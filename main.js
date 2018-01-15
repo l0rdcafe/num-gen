c16b056db5bae7cd1c9cd849a7af1179422db4e6
Ismail Arafa <ismailarafa18@gmail.com>
2018-01-15 22:59:34 +0200

    change doubleNum()

--- main.js
+++ main.js
@@ -3,26 +3,24 @@ const model = {
     numVal: 0
   }
 };
-
 const doubledNumGen = function*(n = 0) {
   for (n; ; n += 1) {
     yield n * 2;
   }
 };
-
 const view = {
   render(val) {
     document.getElementById("num").innerHTML = val;
   }
 };
 
+const genObj = doubledNumGen();
+genObj.next();
+
 const handlers = {
   btnListener() {
     function doubleNum() {
-      const genObj = doubledNumGen();
-      genObj.next();
       const { value } = genObj.next();
-      genObj.next(value);
       model.state.numVal = value;
       view.render(model.state.numVal);
     }
