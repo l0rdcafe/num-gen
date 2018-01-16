9cb59e8f3c5eb96f06ca33fa1143fbe16f7fe851
Ismail Arafa <ismailarafa18@gmail.com>
2018-01-16 18:06:39 +0200

    start bdayGen build

--- main.js
+++ main.js
@@ -1,38 +1,132 @@
-const model = {
-  state: {
-    numVal: 0
+const genFuncs = (function() {
+  class Iterator {
+    constructor(items) {
+      this.index = 0;
+      this.items = items;
+    }
+
+    first() {
+      this.reset();
+      return this.next();
+    }
+    next() {
+      return this.index[(this.index += 1)];
+    }
+    hasNext() {
+      return this.index <= this.items.length;
+    }
+    reset() {
+      this.index = 0;
+    }
+    each(callback) {
+      for (let item = this.first(); this.hasNext(); item = this.next()) {
+        callback(item);
+      }
+    }
   }
-};
-const doubledNumGen = function*() {
-  let n = 0;
-  while (true) {
-    if (n >= 1) {
-      yield n;
-      n *= 2;
-    } else {
+
+  const take = function*(n, iterable) {
+    for (const x of iterable) {
+      if (n <= 0) {
+        return;
+      }
+      n -= 1;
+      yield x;
+    }
+  };
+
+  const cycle = function*(array) {
+    for (let i = 0; ; i += 1) {
+      if (i === array.length) {
+        i = 0;
+      }
+      yield array[i];
+    }
+  };
+
+  const count = function*(n) {
+    while (true) {
       yield n;
       n += 1;
     }
-  }
-};
-const view = {
-  render(val) {
-    document.getElementById("num").innerHTML = val;
-  }
-};
+  };
 
-const genObj = doubledNumGen();
-genObj.next();
+  const repeat = function*(elem, n) {
+    let result = "";
+    for (let i = 0; i < n; i += 1) {
+      result += elem;
+    }
+    yield result;
+  };
+
+  return {
+    count,
+    take,
+    repeat,
+    Iterator,
+    cycle
+  };
+})();
+
+const model = (function() {
+  const doubledNumGen = function*() {
+    let n = 1;
+    while (true) {
+      yield n;
+      n *= 2;
+    }
+  };
+  const numGenObj = doubledNumGen();
+  numGenObj.next();
+
+  const bdayGen = function*(day, month) {
+    while (true) {
+      const bday = moment()
+        .date(day)
+        .month(month);
+      yield bday;
+      bday.add(1, "years");
+    }
+  };
+
+  return {
+    numGenObj,
+    bdayGen
+  };
+})();
+
+const view = {
+  renderNum(val) {
+    document.getElementById("numText").innerHTML = val;
+  },
+  renderNextBday(val) {
+    document.getElementById("bdayText").innerHTML = val;
+  }
+};
 
 const handlers = {
-  btnListener() {
+  doubleBtnListener() {
     function doubleNum() {
-      const { value } = genObj.next();
-      model.state.numVal = value;
-      view.render(model.state.numVal);
+      const { numGenObj } = model;
+      const { value } = numGenObj.next();
+      view.renderNum(value);
     }
-    document.querySelector(".button").addEventListener("click", doubleNum);
+    document.getElementById("numBtn").addEventListener("click", doubleNum);
+  },
+  nextBdayListener() {
+    function nextBday() {
+      const { bdayGen } = model;
+      const monthVal = document.getElementById("monthInput").value - 1;
+      const dayVal = document.getElementById("dayInput").value;
+      if (monthVal !== "" && dayVal !== "") {
+        const bdayGenObj = bdayGen(dayVal, monthVal);
+        const { value } = bdayGenObj.next();
+        view.renderNextBday(value.format("dddd D MMMM YYYY"));
+      }
+    }
+    document.getElementById("bdayBtn").addEventListener("click", nextBday);
   }
 };
 
-handlers.btnListener();
+handlers.doubleBtnListener();
+handlers.nextBdayListener();
